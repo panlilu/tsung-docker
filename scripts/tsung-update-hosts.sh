54e957484ac4990ff1e321ef4cc08ef05e9b644c
panlilu <panlilu@gmail.com>
2016-11-08 18:04:48 +0800

    remove dig

--- scripts/tsung-update-hosts.sh
+++ scripts/tsung-update-hosts.sh
@@ -14,7 +14,7 @@ MARATHON_URL=$(cat /etc/tsung/marathon_url)
 if [[ -n "${MARATHON_URL}" ]]; then
     echo "Discovering tsung-slaves ..."
     #1. discover new tsung_slaves
-    curl -s ${MARATHON_URL}/v2/apps/${tsung_slave_app_name} | grep -Po '"host":"\K.*?(?=")' | xargs dig +short | awk '{ print $1" tsung_slave"NR}' > /etc/tsung/tsung_slaves_hosts.conf
+    curl -s ${MARATHON_URL}/v2/apps/${tsung_slave_app_name} | grep -Po '"host":"\K.*?(?=")' | awk '{ print $1" tsung_slave"NR}' > /etc/tsung/tsung_slaves_hosts.conf
     #2. clean-up previous discovered tsung_slaves
     sed -r '/tsung_slave/d' /etc/hosts > /etc/hosts-temp
     # docker down not allow modifying /etc/hosts and the next line works around that
@@ -24,7 +24,7 @@ if [[ -n "${MARATHON_URL}" ]]; then
     cat /etc/tsung/tsung_slaves_hosts.conf
     echo "Discovering tsung-master ..."
     #1. discover tsung_master
-    curl -s ${MARATHON_URL}/v2/apps/${tsung_master_app_name} | grep -Po '"host":"\K.*?(?=")' | xargs dig +short | awk '{ print $1" tsung_master"}' > /etc/tsung/tsung_master_hosts.conf
+    curl -s ${MARATHON_URL}/v2/apps/${tsung_master_app_name} | grep -Po '"host":"\K.*?(?=")' | awk '{ print $1" tsung_master"}' > /etc/tsung/tsung_master_hosts.conf
     #2. clean-up previous discovered tsung_master
     sed -r '/tsung_master/d' /etc/hosts > /etc/hosts-temp
     # docker down not allow modifying /etc/hosts and the next line works around that
@@ -34,4 +34,3 @@ if [[ -n "${MARATHON_URL}" ]]; then
     cat /etc/tsung/tsung_master_hosts.conf
     echo "DONE."
 fi
-
